f8288daa3378db4ceea7680c87c67f240aa446a0
junjie.xun@oriente.com-at-120896031351 <junjie.xun@oriente.com-at-120896031351>
2019-01-17 08:57:26 +0700

    add testdemo for try

---
+++ tests/punitdemo.py
@@ -0,0 +1,15 @@
+import prettytable
+
+if __name__ == '__main__':
+    print('hello world')
+    t = prettytable.PrettyTable()
+    l1 = [1, 2, 3]
+    l2 = [4, 5, 6]
+    l3 = [4, 3, 1]
+    # t.add_row(l1)
+    # t.add_row(l2)
+    # t.add_row(l3)
+    t.add_column('f1', l1)
+    t.add_column('f2', l2)
+    t.add_column('f3', l3)
+    print(t)
